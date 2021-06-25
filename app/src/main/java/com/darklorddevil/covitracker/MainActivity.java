eb8d4b8dfc56be97a5c35c1fba93ff085802843f
DarkLordDevil <chaitanyanitpawar@gmail.com>
2021-06-25 13:08:27 +0530

    Initial commit

---
+++ app/src/main/java/com/darklorddevil/covitracker/MainActivity.java
@@ -0,0 +1,29 @@
+package com.darklorddevil.covitracker;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.nextbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startApp();
+            }
+        });
+    }
+    public void startApp(){
+        Intent intent=new Intent(this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
